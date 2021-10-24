40c87ceb78e2608c5d8c07543b1f07754211c667
Vladimir <84938807+Veezys@users.noreply.github.com>
2021-10-24 18:06:19 +0300

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: breadyma <breadyma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/13 16:40:10 by breadyma          #+#    #+#             */
+/*   Updated: 2021/10/22 13:53:06 by breadyma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_sp(char	*st_str);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
